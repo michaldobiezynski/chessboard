4b5d89a2e831c47f460e31e412875a7f15c55fc7
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-04-07 10:41:20 +0100

    add template, script and style to the sidebar component - render the clicked squares in an ordered list
    add template, script and style to the sidebar component - render the clicked squares in an ordered list

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sidebar">
+    <h2>Clicked squares</h2>
+    <ol>
+      <li v-for="(square, index) in clickedSquares" :key="square + index">
+        {{ square }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    clickedSquares: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style scoped>
+.sidebar {
+  padding: 1rem;
+  background-color: #e5e5e5;
+  width: 100%;
+}
+
+h2 {
+  margin-top: 0;
+}
+
+ol {
+  padding-left: 1.5rem;
+}
+</style>
