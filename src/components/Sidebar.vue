482f56fbdc3cb29276cb2ed8372ad5e42c9a0b37
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-04-07 10:48:29 +0100

    make the ordered list in the Sidebar component scrollable
    make the ordered list in the Sidebar component scrollable

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="sidebar">
     <h2>Clicked squares</h2>
-    <ol>
-      <li v-for="(square, index) in clickedSquares" :key="square + index">
-        {{ square }}
-      </li>
-    </ol>
+    <div class="list-container">
+      <ol class="scrollable-list">
+        <li v-for="(square, index) in clickedSquares" :key="square + index">
+          {{ square }}
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
@@ -22,16 +24,26 @@ export default {
 
 <style scoped>
 .sidebar {
+  display: flex;
+  flex-direction: column;
   padding: 1rem;
   background-color: #e5e5e5;
   width: 100%;
+  height: 100%;
 }
 
 h2 {
   margin-top: 0;
 }
 
-ol {
-  padding-left: 1.5rem;
+.list-container {
+  flex-grow: 1;
+  overflow-y: auto;
+  margin-bottom: 2rem;
+}
+
+.scrollable-list {
+  margin-left: 1.5rem;
+  height: 100%;
 }
 </style>
