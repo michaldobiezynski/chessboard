a9ddc98f37523dd16fce8afc77830f4e7d9df327
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-04-07 10:01:46 +0100

    add initial boilerplate template and script code to the Chessboard component
    add initial boilerplate template and script code to the Chessboard component

---
+++ src/components/Chessboard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="chessboard">
+      <div
+        v-for="(row, rowIndex) in 8"
+        :key="rowIndex"
+        class="chessboard-row"
+      >
+        <div
+          v-for="(col, colIndex) in 8"
+          :key="colIndex"
+          :class="['chessboard-square', (rowIndex + colIndex) % 2 === 0 ? 'light' : 'dark']"
+          @click="squareClicked(rowIndex, colIndex)"
+        ></div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      squareClicked(rowIndex, colIndex) {
+        this.$emit("square-clicked", { rowIndex, colIndex });
+      },
+    },
+  };
+  </script>
